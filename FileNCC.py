081c534cca18f7b6d079b553dba7ef46268e82be
Isteak <143406194+iste4k@users.noreply.github.com>
2023-08-30 16:26:35 +0600

    Initial commit

--- FileNCC.py
+++ FileNCC.py
@@ -1,24 +1,27 @@
 # importing essential modules
 import os   # importing os module
+import itertools
 
 
 # identifying paths and other variables
 host_path = os.getcwd() # gets the current directory path
 directory_path = input("Host directoreh: ").replace(" ", "_")   # the host directory path
-total_files = 0 # number of total files in the host directory
+# total_files = 0 # number of total files in the host directory
 user_input = input("Name format: ").replace(" ", "_")
+old_list = []
+new_list = []
 
 fd_list = os.listdir(directory_path)    # list of files and directories
 
 
 # counting the total number of files
-for file_name in fd_list:
-    if "." in file_name:    # it will ignore the directories in the host directory
-        total_files += 1
+for file_num, file_name in enumerate(fd_list):
+    # if "." in file_name:    # it will ignore the directories in the host directory
+    #     total_files += 1
 
     # identify the code from user input
     if "N+N" in user_input:
-        new_file_name = host_path + "\\" + directory_path + "\\" + user_input.replace("N+N", str(total_files))
+        new_file_name = host_path + "\\" + directory_path + "\\" + user_input.replace("N+N", str(file_num+1))
 
     # will look into this feature later
     # generate decreasing numbers
@@ -29,6 +32,13 @@ for file_name in fd_list:
     
     # generating the existing files' paths
     file_directory = host_path + "\\" + directory_path + "\\" + file_name
+    new_file_directory = new_file_name.replace("_", " ")
 
-    # change the file names
-    os.rename(file_directory, new_file_name.replace("_", " "), FileExistsError=False)
+    # storing old and new paths and file names
+    old_list.append(file_directory)
+    new_list.append(new_file_directory)
+
+
+# changing the file names
+for (old_name, new_name) in zip(old_list, new_list):
+    os.rename(old_name, new_name)
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-t:
-#     for file_path in os.scandir(dir_path):
-#         if file_path.is_file():
-#             file_count += 1
-#             print(os.path.basename(dir_path))
