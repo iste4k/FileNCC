0420175ad833db449464f759c90b659c03419b5a
Isteak <143406194+iste4k@users.noreply.github.com>
2023-08-30 15:43:02 +0600

    Project target accomplished

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os   # importing os module
 
 # identifying paths and other variables
 host_path = os.getcwd()
-directory_path = "TestFolder"   # the host directory path
+directory_path = "TestFolder".replace(" ", "_")   # the host directory path
 total_files = 0 # number of total files in the host directory
-user_input = "True Detective S01EN+N.txt"
+user_input = "Mr Robot S01EN+N.txt".replace(" ", "_")
 
 fd_list = os.listdir(directory_path)    # list of files and directories
 
@@ -23,4 +23,4 @@ for file_name in fd_list:
     
     file_directory = host_path + "\\" + directory_path + "\\" + file_name
 
-    os.rename(file_directory, new_file_name)
+    os.rename(file_directory, new_file_name.replace("_", " "))
