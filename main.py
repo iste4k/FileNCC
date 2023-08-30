d9ccd18a972a665b352ac845fdb9c39662826d46
Isteak <143406194+iste4k@users.noreply.github.com>
2023-08-30 14:56:06 +0600

    Project cleaning

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import os   # importing os module
 
 
 # identifying paths and other variables
+host_path = os.getcwd()
 directory_path = "TestFolder"   # the host directory path
 total_files = 0 # number of total files in the host directory
-user_input = "Fight Club S01EN+N"
-new_file_names = []
+user_input = "Fight Club S01EN+N.txt"
 
 fd_list = os.listdir(directory_path)    # list of files and directories
+
 
 # counting the total number of files
 for file_name in fd_list:
@@ -16,11 +17,10 @@ for file_name in fd_list:
         total_files += 1
 
     if "N+N" in user_input:
-        new_file_name = user_input.replace("N+N", str(total_files))
+        new_file_name = host_path + "\\" + directory_path + "\\" + user_input.replace("N+N", str(total_files))
     elif "N-N" in user_input:
-        new_file_name = user_input.replace("N-N", str(total_files))
+        new_file_name = host_path + "\\" + directory_path + "\\" + user_input.replace("N-N", str(total_files))
     
-    new_file_names.append(new_file_name)
+    file_directory = host_path + "\\" + directory_path + "\\" + file_name
 
-
-print(new_file_names)
+    os.rename(file_directory, new_file_name)
