1ec3debb7298489ee89c5ebbc808b7281cd21421
Isteak <143406194+iste4k@users.noreply.github.com>
2023-08-30 15:13:18 +0600

    Initial commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os   # importing os module
 host_path = os.getcwd()
 directory_path = "TestFolder"   # the host directory path
 total_files = 0 # number of total files in the host directory
-user_input = "Fight Club S01EN+N.txt"
+user_input = "True Detective S01EN+N.txt"
 
 fd_list = os.listdir(directory_path)    # list of files and directories
 
@@ -16,10 +16,10 @@ for file_name in fd_list:
     if "." in file_name:    # it will ignore the directories in the host directory
         total_files += 1
 
-    if "N+N" in user_input:
+    if "N+N" in user_input: # 
         new_file_name = host_path + "\\" + directory_path + "\\" + user_input.replace("N+N", str(total_files))
     elif "N-N" in user_input:
-        new_file_name = host_path + "\\" + directory_path + "\\" + user_input.replace("N-N", str(total_files))
+        new_file_name = host_path + "\\" + directory_path + "\\" + user_input.replace("N-N", str(abs(len(fd_list) - (total_files+1))))
     
     file_directory = host_path + "\\" + directory_path + "\\" + file_name
 
